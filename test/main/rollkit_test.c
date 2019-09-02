685e49c32d7b771bba1bd458017696c9a0ca543c
Neil Betham <nbetham@me.com>
2019-09-02 02:33:37 -0700

    libtommath needs lots of stack space

--- test/main/rollkit_test.c
+++ test/main/rollkit_test.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include "unity.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 static void print_banner(const char* text);
 
+void run_tests() {
+  print_banner("Running all the registered tests");
+  UNITY_BEGIN();
+  unity_run_all_tests();
+  UNITY_END();
+  while(1) { vTaskDelay(10); }
+}
+
 void app_main()
 {
-    print_banner("Running all the registered tests");
-    UNITY_BEGIN();
-    unity_run_all_tests();
-    UNITY_END();
+  TaskHandle_t test_task_handle = NULL;
+  xTaskCreate(
+    run_tests,
+    "Tests",
+    20000,
+    NULL,
+    tskIDLE_PRIORITY,
+    &test_task_handle
+  );
+  while(1) { vTaskDelay(10); }
 }
 
 static void print_banner(const char* text)
