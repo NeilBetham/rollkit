c8f5c87c7b1d1836b5a3af56bc6d96dcb02b5f5b
Neil Betham <nbetham@me.com>
2020-04-11 23:39:40 -0700

    Move main to examples folder

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# rollkit
-ESP32 application for implementing a HomeKit accessory.
+rollkit
+========
+ESP IDF components for implementing a HomeKit accessory.
+
+## Quick Start
+1. In project cmake add `include(<path_to_rollkit>/CMakeLists.txt)`
+2. Add `rollkit` to required components
+3. Implement your Accessory
+
+You can also look at the `simple_switch` in the examples folder to get an idea of how to implement an Accessory from scratch.
+
+## Details
+Rollkit will handle all the background communication between the HomeKit controller and your code. Currently rollkit will take two functions for reading and writing to the state of your device.
+
+*Note: rollkit doesn't currently support updating state externally from event sources other than the paired controlling device - this functionality is in the works*
+
+### Implementing an Accessory
+*Some general notes from having gone through the process*
+
+Likely the best place to start is by getting a copy of the HomeKit Accessory Protcol specification. It's *free* ish and will serve as a decent document to get info on what Services are available and what Services require what Characteristics.
+
+Knowing this will be key if you want the iOS controller to properly display your Accessory in the iOS HomeKit app. Rest assured if you're seeing issue where the Accessory is listed as "Not Responding" then it is likely due to a miss-configuration on the Accessory side.
+
+If you're having trouble implementing an Accessory and it's not showing up in the HomeKit app it's time to consult `Console` on a Mac. Once you have the device plugged in via USB and configured with Xcode you should be able to start viewing logs from the device. A simple filter for the `homed` process will help narrow down the messages you have to look at.
+
+### Component Breakdown
+ - `external` - Just houses code for mongoose and the json lib
+ - `libtommath` - Used for all the big integer math
+ - `srp` - My C++ implementation of Stanford's SRP protocol
+ - `rollkit` - All the code related to implementing a HomeKit Accessory
+
+### Caveat
+I am __not__ a cryptography expert; as such I make __no__ guarantees about the security of any Accessories implemented with this library. I did have to implement some of the crypto for this library by hand. For those elements I have added unit tests from RFC test vectors to help ensure correctness. Beyond that however, I am out of my depth.
+
+## TODO
+ - [ ] External event feedback to controllers
+ - [ ] Better Accessory sanity checking
+ - [ ] Easier DSL for configuring accessories
---
+++ examples/simple_switch/main/main.cpp
@@ -0,0 +1,109 @@
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <esp_wifi.h>
+#include <nvs_flash.h>
+#include <sodium.h>
+#include <lwip/err.h>
+#include <lwip/sys.h>
+
+#include "sdkconfig.h"
+
+#include "rollkit.hpp"
+#include "config.hpp"
+
+using namespace std;
+
+rollkit::App app;
+rollkit::Accessory acc;
+rollkit::Service acc_switch;
+rollkit::Characteristic acc_switch_on_char;
+rollkit::Characteristic acc_switch_name_char;
+
+bool switch_on = false;
+
+
+void init_app() {
+  app.init(ACC_NAME, ACC_MODEL, ACC_MANUFACTURER, ACC_FIRMWARE_REVISION, ACC_SETUP_CODE);
+
+  acc_switch_on_char = rollkit::Characteristic(
+    APPL_CHAR_UUID_ON,
+    [](nlohmann::json v){ ESP_LOGD("acc", "Switch On Write: %d", v.get<int>()); switch_on = (bool)v.get<int>(); },
+    []() -> nlohmann::json { ESP_LOGD("acc", "Switch On Read: %d", switch_on); return switch_on; },
+    "bool",
+    {"pr", "pw", "ev"}
+  );
+  acc_switch_name_char = rollkit::Characteristic(
+    APPL_CHAR_UUID_NAME,
+    [](nlohmann::json v){},
+    []() -> nlohmann::json { ESP_LOGD("acc", "Switch Name Read"); return "Pump"; },
+    "string",
+    {"pr"}
+  );
+  acc_switch = rollkit::Service(
+    APPL_SRVC_UUID_SWITCH,
+    false,
+    true
+  );
+  acc_switch.register_characteristic(acc_switch_on_char);
+  acc_switch.register_characteristic(acc_switch_name_char);
+  acc.register_service(acc_switch);
+
+  app.register_accessory(acc);
+  app.start();
+}
+
+
+// ESP32 WiFi handler.
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    wifi_event_sta_disconnected_t* data = (wifi_event_sta_disconnected_t*)event_data;
+    esp_wifi_connect();
+    ESP_LOGI("main", "Disconnected(%d): retyring connection to WiFi network...", data->reason);
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+    ESP_LOGI("main", "Connected: IP: " IPSTR, IP2STR(&event->ip_info.ip));
+    init_app();
+  }
+}
+
+
+void init_wifi() {
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+  wifi_config_t wifi_config;
+  memset(&wifi_config, 0, sizeof(wifi_config));
+  strcpy((char*)wifi_config.sta.ssid, WIFI_SSID);
+  strcpy((char*)wifi_config.sta.password, WIFI_PASS);
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI("main", "WiFi Init finished.");
+}
+
+
+extern "C" {
+	void app_main(void);
+}
+void app_main(void) {
+  if(sodium_init() == -1){
+    ESP_LOGE("Crypto", "libsodium failed ot initilize");
+    esp_restart();
+  } else  {
+    ESP_LOGD("Crypto", "libsodium initialized");
+  }
+  nvs_flash_init();
+  init_wifi();
+}
